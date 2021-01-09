c9f812b8ea91fcf89849c144c864f9ac37e3cf43
Bir Vincent <vincentbir@hotmail.com>
2021-01-09 09:38:18 +0100

    Ajout des dépendances ui-kitten

--- App.js
+++ App.js
@@ -3,14 +3,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/navigation/Navigation';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry, Layout, Button } from '@ui-kitten/components';
+import { default as theme } from './theme.json';
+import { default as mapping } from './mapping.json';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 export default function App() {
+  
   return (
-    <NavigationContainer>
-      <Navigation />
-      <StatusBar style="auto" />
-    </NavigationContainer>
+
+      <ApplicationProvider 
+      {...eva} 
+      theme={{ ...eva.light, ...theme }}
+      customMapping={mapping}>
+        <NavigationContainer>
+          <Navigation />
+          <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Button>HOME</Button>
+          </Layout>
+        </NavigationContainer>
+      </ApplicationProvider>
+    
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -21,3 +38,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
