e3d16637d2196ca85f47d939034cf2bae47e6c5b
Clément Chevalet <clement.chevalet@gmail.com>
2021-03-04 19:39:39 +0100

    premiere flexbox page meteo

--- src/components/MeteoInformations.js
+++ src/components/MeteoInformations.js
@@ -24,6 +24,11 @@ const MeteoInformations = ({ route }) => {
         <View style={styles.mainView}>
           <View style={styles.currentGlobalInfos}>
             <Text style={styles.city}>{route.params.city}</Text>
+            <Text style={styles.weatherTemperature}>Pluie, 13°C</Text>
+            <View style={styles.currentWeatherInfos}>
+              <Text style={styles.textCurrentWeatherInfos}>10°C    19°C</Text>
+              <Text style={styles.textCurrentWeatherInfos}>31%    6km/h    35%</Text>
+            </View>
           </View>
 
           <View style={styles.middle1}>
@@ -86,5 +91,20 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: 18,
+  },
+
+  weatherTemperature: {
+    fontSize: 20,
+    fontWeight : 'bold',
+  },
+
+  currentWeatherInfos : {
+    position: 'absolute',
+    bottom: 0,
+  },
+
+  textCurrentWeatherInfos : {
+    fontSize: 15,
+    fontWeight : 'bold',
   }
 });
